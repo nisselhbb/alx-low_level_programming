2c581e03fa625c8c6cbebab51a55ed2aec4290c3
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-17 13:18:57 -0700

    digits from 0 to 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main
- * Description: 'prints all single digit numbers to 9 starting from 0'
+ * main - Enrty point
+ *Description: 'Print all single digit numbers starting from 0 to 9'
  * Return: Always 0 (success)
  */
 int main(void)
