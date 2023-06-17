f38369145d6460338398d44bded5f22794f50333
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-17 12:36:13 -0700

    print all single digit from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: :Always 0 (success)
+ */
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
