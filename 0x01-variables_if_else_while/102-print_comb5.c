555757ef590b17aeefb51d63652dbfd1c8a19b4b
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-17 20:06:18 -0700

    two two digit combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+	int d;
+
+	for (c = 0; c < 99; c++)
+	{
+		for (d = 0; d < 100; d++)
+		{
+		putchar(c / 10 + '0');
+		putchar(c % 10 + '0');
+		putchar(' ');
+		putchar(d / 10 + '0');
+		putchar(d % 10 + '0');
+
+		if (c != 98 || d != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
