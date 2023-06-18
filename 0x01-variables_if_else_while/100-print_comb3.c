57dabd820606889d141149ec1550123e3d15b996
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-17 19:26:23 -0700

    combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a < 10; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = a + '1'; b < 10; b++)
+		for (b = a + 1; b < 10; b++)
 		{
 			putchar(a + 48);
 			putchar(b + 48);
 
-			if (a != '8' || b != '9')
+			if (a != 8 || b != 9)
 			{
 				putchar(',');
 				putchar(' ');
