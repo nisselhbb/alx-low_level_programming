4f5962ff5dcb4886587a2f14263c7f825c0024f7
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-17 19:41:45 -0700

    combo of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 7; a++)
 	{
-		for (b = a + 1; b < 10; b++)
+		for (b = a + 1; b <= 8; b++)
 		{
-			for (c = b + 1; c < 10; c++)
+			for (c = b + 1; c <= 9; c++)
 			{
 				putchar(a + 48);
 				putchar(b + 48);
 				putchar(c + 48);
 
-				if (a != 8 || b != 9 || c != 9)
+				if (a != 7 || b != 8 || c != 9)
 				{
 					putchar(',');
 					putchar(' ');
