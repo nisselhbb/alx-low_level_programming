d9cbad21e1d66e0788721a5ae95d101a5bc538f7
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-20 16:51:56 -0700

    printing the alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@ void print_alphabet_x10(void)
 	char letter;
 	int a;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (a = 1; a <= 10; a++)
 	{
-		for (a = 1; a <= 10; a++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
