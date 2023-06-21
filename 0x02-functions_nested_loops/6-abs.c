cb81f6c60fed2f024a6e6c01a5b83111fe104a63
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-21 07:02:16 -0700

    absolute value on an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Enrty point
+ * @n: the integer
+ *
+ * Return: the absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		_putchar('n');
+		return (n);
+	}
+	else
+	{
+		_putchar("n * -1");
+		return (n * -1);
+	}
+	_putchar('\n');
+}
