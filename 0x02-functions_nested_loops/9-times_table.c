f9995c2fd72109b2485804fb60cf69e7dd878ae4
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-21 13:30:57 -0700

    multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ *
+ * Return: no return
+ */
+void times_table(void)
+{
+	int number;
+	int count;
+
+	for (number = 0; number <= 9; number++)
+	{
+		for (count = 0; count <= 9; count++)
+		{
+			_putchar(number * 9);
+			_putchar('\n');
+		}
+	}
+}
