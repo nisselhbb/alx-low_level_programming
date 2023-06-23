598b31705fef88c7babcafb772cf3af40e3a4e25
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-22 18:38:37 -0700

    printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: none
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n');
+}
