0050fada321af18d896ac18c572c72f49e4fdcb9
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-22 18:05:45 -0700

    checking for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#inlude "main.h"
+
+/**
+ * _isupper - Entry point (check for uppercase character)
+ * @c: character to be checked
+ *
+ * Return: Always 0 (success)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
