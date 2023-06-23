3b32aeb786e8fb661446609a16ecdc4c63e8a91c
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-23 10:46:57 -0700

    printing more numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,9 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if (n > 9)
+			if (n >= 10)
 			{
 				_putchar((n / 10) + '0');
-			}
-			else
-			{
 				_putchar((n % 10) + '0');
 			}
 		}
