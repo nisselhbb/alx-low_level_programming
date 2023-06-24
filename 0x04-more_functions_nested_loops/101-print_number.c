f6a77f91165031592eae7577cd6b68106f27a564
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-23 18:10:15 -0700

    printing integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,9 +4,36 @@
  * print_number - prints integers
  * @n: integer to be printed
  *
- * Return: the integer
+ * Return: no return
  */
 void print_number(int n)
 {
-	return (n);
+	unsigned int a = n;
+	unsigned int b = n;
+	unsigned int c = n;
+	double d = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		if (n < 0)
+		{
+			c = n * -1;
+			_putchar('-');
+		}
+		while (d <= c)
+			d = d * 10;
+		a = d / 10;
+
+		while (a >= 1)
+		{
+			b = c / a;
+			_putchar(b + '0');
+			c = (c - (a * b));
+			a = a / 10;
+		}
+	}
 }
