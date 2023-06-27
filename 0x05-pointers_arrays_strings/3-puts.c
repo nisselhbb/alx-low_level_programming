5898543423a5b9aa8f4c7f85ec4eb4cb60b10a38
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-26 17:43:23 -0700

    printing a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints a string to stdout followed by a new line
+ * @str: the string to be printed
+ *
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	puts(str);
+	_putchar('\n');
+}
