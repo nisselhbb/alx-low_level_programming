a6ece557f04bf243a530bbd0c44fab810f1e0955
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-27 17:42:05 -0700

    printing the 2nd half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,6 +13,10 @@ void puts_half(char *str)
 	int half = len / 2;
 	int index;
 
+	if (len % 2 != 0)
+	{
+		half = (len - 1) / 2;
+	}
 	for (index = half; index < len; index++)
 	{
 		_putchar(str[index]);
