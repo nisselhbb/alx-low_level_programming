2db7349e89c3567b2faa459f929b4f9369d9f0ae
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-29 18:30:03 -0700

    concatenating two strings with n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest_length != '\0')
 		dest_length++;
-	for (i = 0; i < n && src[i] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_length + i] = src[i];
 	dest[dest_length + i] = '\0';
 	return (dest);
