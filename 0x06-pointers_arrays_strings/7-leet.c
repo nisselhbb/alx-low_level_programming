4d875f6c4b1f77c24976896beab53f222d6c05b3
nisselhbb <nissrineelhobbi@gmail.com>
2023-06-30 19:51:56 -0700

    turning letters of a string into Leet characters

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - turns letters os a string into leet
+ * @s: pointer to the string
+ *
+ * Return: the string
+ */
+char *leet(char *s)
+{
+	int i, j;
+	int letters[] = "AaEeOoTtLl";
+	int leet[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = leet[j];
+			}
+		}
+	}
+	return (s);
+}
