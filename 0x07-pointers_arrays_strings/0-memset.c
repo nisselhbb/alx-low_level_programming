8e94259058c0d0af43742ad4cc2310bdf33f22e0
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-03 09:59:35 -0700

    filling memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,11 +2,11 @@
 
 /**
  * _memset - fills memory with a constant byte
- * @s: pointer to the string
+ * @s: pointer to the memory area
  * @b: constant byte
- * @n: bytes of memory in the string
+ * @n: bytes of memory
  *
- * Return: the string
+ * Return: pointer to s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
