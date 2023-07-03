a884cc697f8e402aea87456bf9f340fe2af66318
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-03 14:29:31 -0700

    getting the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -21,7 +21,7 @@ unsigned int _strspn(char *s, char *accept)
 				count++;
 				break;
 			}
-			else if (accept[r + 1] == '\0')
+			else if (accept[index + 1] == '\0')
 				return (count);
 		}
 		s++;
