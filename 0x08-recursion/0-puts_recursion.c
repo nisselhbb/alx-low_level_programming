e4e93387dae6317caa69cb711d2466a912c426a8
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-05 13:29:54 -0700

    printing a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer to the string
+ * Return: no return
+ */
+void _puts_recursion(char *s)
+{
+	if ([s] != '\0')
+		_putchar(s);
+	_putchar('\n');
+}
