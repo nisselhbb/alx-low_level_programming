0619cb56162a2ce5bf248d4dc270b4f65b09e29a
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-05 16:20:31 -0700

    returning the factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,11 +3,14 @@
 /**
  * factorial - Entry point
  * @n: pointer to the given number
- * Return: -1 if error,if not factorial of n
+ * Return: -1 if error,1 if it's 0 or 1
+ * if not factorial of n
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
+	if (n == 0 || n == 1)
+		return (1);
 	return (n * factorial(n - 1));
 }
