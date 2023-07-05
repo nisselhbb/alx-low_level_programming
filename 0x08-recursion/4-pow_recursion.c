6128eb7ae7539ee81ada2a0f2a0dfb8fe782eeff
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-05 16:34:58 -0700

    returning the value of x raised by y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of a number
+ * raised by another number
+ * @x: the number
+ * @y: number of times x to be multiplied
+ * Return: -1 if error, if not the value of
+ * x raised by y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (x * factorial(x * *y));
+}
