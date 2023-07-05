7f2a4390615b0c24b3cc1ffef6e6a87de2e4cb7c
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-05 14:56:19 -0700

    printing a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,17 +7,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
-
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	for (i = i - 1; i >= 0; i--)
-	{
-		_putchar(*s);
-		_print_rev_recursion(s - 1);
-	}
-
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
