049ff395d2bd8f1518136bf43c1df563253f952a
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-07 16:33:46 -0700

    writing a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: the number of command line
+ * arguments
+ * @argv: the program command line
+ * arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
