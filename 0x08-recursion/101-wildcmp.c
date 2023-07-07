e56a7e56220fa7db6a0056900e5af10292b4e1f2
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-06 17:11:33 -0700

    comparing two strings using recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings if they are
+ * identical
+ * @s1: pointer to the 1st string
+ * @s2: pointer to the 2nd string that can contain *
+ * as a special character
+ * Return: 1 if the strings are identical, 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && (s2 + 1) != '\0' && *s1 == '\0')
+	{
+		return (0);
+	}
+	else if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1 || s1 + 1, s2));
+	}
+	return (0);
+}
+
