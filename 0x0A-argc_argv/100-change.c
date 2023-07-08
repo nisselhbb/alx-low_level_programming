baf2772c4207112f6a2dc9d48d50ebd8e8f3b45c
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-07 18:46:59 -0700

    writing program that prints the minimum number of coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of command line
+ * arguments
+ * @argv: the program command line
+ * arguments
+ * Return: 1 if incorrect, 0 if the opposite
+ */
+int main(int argc, char *argv[])
+{
+	int cents[] = {25, 10, 5, 2, 1};
+	int i = 0;
+	int count = 0;
+	int j;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	if (a < 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (j = 0; j < 5; j++)
+	{
+		count = count + i / cents[j];
+		i = i % cents[j];
+	}
+	printf("%j\n", cents);
+	return (0);
+}
