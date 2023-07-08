c7e4784221bfe354e0d5cf514dc02b72bb4618f4
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-07 18:24:37 -0700

    writing a program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * @argc: number of command line
+ * arguments
+ * @argv: the program command line
+ * arguments
+ * Return: 1 if wrong, 0 if correct
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 1;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		int j = 0;
+
+		while (argv[i][j] != '\0')
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
