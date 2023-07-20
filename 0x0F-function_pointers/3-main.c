5bf6b6bd0e9da9fb22815945a255aa88564cefa9
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-19 17:30:55 -0700

    a hard program that perform simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+/**
+ * main - Entr point
+ * @argv: the number of command line
+ * arguments
+ * @argc: tthe command line arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	int (*main_op)(int, int);
+
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	main_op = get_function_op(argv[2]);
+	if (!main_op)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (!num2 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", main_op(num1, num2));
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two integers
+ * @a: 1t integer
+ * @b: 2nd integer
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulu of two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
