46d7ad7feafa81b5a51dec2fa9cdee1840b4d23c
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-20 09:43:14 -0700

    printing a name using a function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef FUNCPTR_H
 #define FUNCPTR_H
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
