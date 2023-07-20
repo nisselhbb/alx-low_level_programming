4f04e104aee486590b2e526e28b669b137b3a042
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-19 18:08:13 -0700

    writing a program that prints the 'opcodes' of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - checks code
+ * @argc: the number of command line
+ * arguments
+ * @argv: the command line arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	char *ptr = (char *)main;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	i = atoi(argv[1]);
+	if (i < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (i--)
+	{
+		printf("%02hhx%s", *ptr++, b ? " " : "\n");
+	}
+	return (0);
+}
