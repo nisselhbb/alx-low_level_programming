9697bbda16579e7e3235e41af17422e24c611b05
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-20 18:23:33 -0700

    somehow writing a function that prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed
+ * to the function
+ * Return: nil, no return
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int n = 0, num;
+	char *j;
+	float f;
+	char c;
+
+	va_start(args, format);
+
+	while (format && format[n])
+	{
+	switch (format[n])
+	{
+		case 'c':
+			c = va_arg(args, int);
+			printf("%c", c);
+			break;
+		case 'i':
+			num = va_arg(args, int);
+			printf("%d", num);
+			break;
+		case 'f':
+			f = va_arg(args, float);
+			printf("%f", f);
+			break;
+		case 's':
+			j = va_args(args, char*);
+			printf("%s", j);
+			break;
+		default:
+			n++;
+			continue;
+	}
+	if (format[n + 1] != '\0')
+	{
+		printf(", ");
+	}
+	n++
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,20 @@
 #ifndef VARIADICFUNC_H
 #define VARIADICFUNC_H
 #include <stdarg.h>
+/**
+ * struct token - struct token
+ * @token: representing a data type
+ * @f: function pointer that points to the
+ * corresponding data type
+ */
+typedef struct token
+{
+	char *token;
+	void (*f)(char*, va_list);
+} token_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
