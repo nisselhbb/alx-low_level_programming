62b19babaa96ffaa7ce29da170ab0ea52892c440
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-27 19:02:46 -0700

    writing a 64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,10 @@
 section .data
-string db "Hello, Holberton",10
-section .text
-global start
+	string db "Hello, Holberton",10
+	section .text
+	global start
 start:
-mov rax,1
-mov rdi,1
-mov rsi, string
-mov rdx, 17
-syscall
+	mov rax,1
+	mov rdi,1
+	mov rsi, string
+	mov rdx, 17
+	syscall
