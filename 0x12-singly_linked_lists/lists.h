fdfd366f8e13f5781657f46d6f514a54fbd006f0
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-27 17:27:29 -0700

    printing all elements of a list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef struct list_t
+typedef struct list_c
 {
 	unsigned int length;
 	char *str;
-	struct list_t *next;
+	struct list_c *next;
 }list_t;
 int _putchar(char c);
 size_t print_list(const list_t *h);
