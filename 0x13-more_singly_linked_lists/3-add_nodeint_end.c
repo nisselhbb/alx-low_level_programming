004a160bd12ddaa57efb550ffb3c6b123fff226a
nisselhbb <nissrineelhobbi@gmail.com>
2023-08-01 14:38:13 -0700

    adding a new node at the end of a linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,13 +17,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		return (NULL);
 	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+		*head = new_node;
 	else
 	{
-		new_node->n = n;
-		new_node->next = NULL;
-
-		if (*head == NULL)
-			*head = new_node;
+		end_node = *head;
+		
 		while (end_node->next != NULL)
 		{
 			end_node = end_node->next;
