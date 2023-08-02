6a9c83f85dca0e0dd85a718d05738fe4de846422
nisselhbb <nissrineelhobbi@gmail.com>
2023-08-01 17:18:12 -0700

    deleting the node at index index of a list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		temp = *haed;
+		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 		return (1);
