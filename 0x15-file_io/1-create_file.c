4cc2693335181b3d2f1a24918ec212fe43258200
nisselhbb <nissrineelhobbi@gmail.com>
2023-08-07 15:38:02 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	for (length = 0; text_file[length]; length++)
+	for (length = 0; text_content[length]; length++)
 		;
 	per = write(file, text_content, length);
 	if (per == -1)
