3027c3c973935aa5b3464152d666eaaea1322579
nisselhbb <nissrineelhobbi@gmail.com>
2023-08-07 15:16:01 -0700

    a function that reads a txt file...

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: pointer to the file
+ * @letters: number of letters it should read
+ * and print
+ * Return: the actual number of letters it could read
+ * and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	char *buffer;
+	size_t fwrite;
+	size_t fread;
+
+	if (filename == NULL)
+		return (0);
+	file = open("filename", O_RDONLY);
+	if (file == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * (letters));
+	if (buffer == NULL)
+		return (0);
+	fread = read(file, buffer, letters);
+	fwrite = write(STDOUT_FILENO, buffer, fread);
+	close(file);
+	free(buffer);
+	return (fwrite);
+}
