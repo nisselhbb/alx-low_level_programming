dd5dc13daa1ae0e53bd553e44efcfb705d42c241
nisselhbb <nissrineelhobbi@gmail.com>
2023-08-07 15:19:56 -0700

    a function that reads a txt file...

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,12 +12,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
 	char *buffer;
-	size_t fwrite;
-	size_t fread;
+	ssize_t fwrite;
+	ssize_t fread;
 
 	if (filename == NULL)
 		return (0);
-	file = open("filename", O_RDONLY);
+	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * (letters));
