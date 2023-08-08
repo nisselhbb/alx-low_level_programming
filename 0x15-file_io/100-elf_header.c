8c86f48d71d356c0d5ac98848647eae1ea142fda
nisselhbb <nissrineelhobbi@gmail.com>
2023-08-07 18:57:22 -0700

    advanced task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,21 @@
-#include "elf.h"
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 /**
  * check_elf - checks if a file is an ELF file
  * @e_ident: pointer to array containing the ELF magic numbers
@@ -231,7 +248,7 @@ void close_elf(int elf)
 	}
 }
 /**
- * main - displays the information contained in the 
+ * main - displays the information contained in the
  * ELF header at the start of an ELF file
  * @argc: the number of arguments given to the program
  * @argv: array of pointerss to the arguments
